9a7f95c29e87adcea11112ba427a5a38e785bdd3
Anna Eklöv <annaeklov@Annas-MacBook-Air.local>
2019-10-29 10:47:52 +0100

    almost done

---
+++ index.js
@@ -0,0 +1,70 @@
+/*HEADER*/
+
+let h1header = document.querySelector("header h1");
+h1header.textContent = "Fruits & Vegetables Corp";
+
+/*LIST and A-TAG*/
+
+let aTag = document.querySelector("header ul:last-child li:last-child a");
+aTag.textContent = "Vegetables";
+aTag.setAttribute("href", "#vegetables");
+
+/*SWAP CONTACT AND ABOUT*/
+
+let mainSection = document.getElementById("main");
+let aboutSection = document.getElementById("about");
+let contactSection = document.getElementById("contact");
+mainSection.insertBefore(aboutSection, contactSection);
+
+/*ABOUT*/
+aboutSection.innerHTML = "";
+
+let h2About = document.createElement("h2");
+aboutSection.appendChild(h2About);
+h2About.textContent = "About"
+
+let pAbout = document.createElement("p");
+aboutSection.appendChild(pAbout);
+pAbout.textContent = "We're the best in fruits & vegetables"
+
+/*CONTACT*/
+
+let pContact = document.querySelector("#contact p");
+let h2Contact = document.createElement("h2");
+contactSection.appendChild(h2Contact);
+contactSection.insertBefore(h2Contact, pContact);
+h2Contact.textContent = "Contact";
+
+/*TABLE*/
+
+let trTable = document.querySelector("thead tr");
+
+let tdTable = document.querySelectorAll("thead td");
+
+console.log(tdTable);
+
+
+/* let thTable = document.createElement("th");
+
+trTable.replaceChild(thTable, tdTable); */
+
+/*INCLUDE CSS FILE*/
+
+let head = document.querySelector("head");
+let cssLink = document.createElement("link");
+
+cssLink.rel = "stylesheet";
+cssLink.type = "text/css";
+cssLink.href = "main.css";
+
+head.appendChild(cssLink);
+
+/*CHANGE HEAD TITLE*/
+
+let titleHead = document.querySelector("head title");
+titleHead.textContent = "Fruits & Vegetables Corp";
+
+/* let h1header = document.querySelector("header h1");
+h1header.textContent = "Fruits & Vegetables Corp"; */
+
+
